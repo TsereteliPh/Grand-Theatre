810f9d8f338a84d4486d3a9c65df1870232d22a4
TsereteliPh <filippok01001@gmail.com>
2022-11-18 23:11:36 +0300

    Старт проекта, сделана разметка, почти стилизован header

---
+++ dist/scripts/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    const burger = $('.nav__button');
+    const popup = $('.nav__popup');
+    const cross = $('.cross-btn');
+    const menu = $('.nav__list').clone();
+    const contacts = $('.contact__links').clone();
+
+    burger.click(function () {
+
+        if (popup.hasClass('nav__popup--hidden')) {
+           popup.removeClass('nav__popup--hidden');
+           popup.append(menu).append(contacts);
+           menu.addClass('js-header-menu');
+           contacts.addClass('js-header-contacts');
+        } else {
+            return
+        }
+    });
+
+    cross.click(function () {
+        if (popup.hasClass('nav__popup--hidden')) {
+            return
+         } else {
+            popup.addClass('nav__popup--hidden');
+         }
+
+    });
+    
+})
