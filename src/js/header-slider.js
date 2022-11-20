dffdba517448cb4d7d7a3aad8902fad81d45167a
TsereteliPh <filippok01001@gmail.com>
2022-11-20 16:36:50 +0300

    Изменен шрифт, доделан блок about. Нужно переделать слайдеры

---
+++ src/js/header-slider.js
@@ -0,0 +1,8 @@
+if (window.screen.width >= 1410) {
+    $('.perf-slider').slick({
+        
+    });
+
+    $('.header__slider').children('.slick-next').text('След ⟶');
+    $('.header__slider').children('.slick-prev').text('⟵ Пред');
+}
